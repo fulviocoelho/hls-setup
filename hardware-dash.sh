943e49081f70a4ef521c8ca9ebfe3c31cbec32ee
Fulvio Coelho <coelho.fulvio@gmail.com>
2025-08-17 20:32:21 -0300

    Adicionado serviço de loja para jogos de switch e dashboard para status de hardware

---
+++ hardware-dash.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock:ro -v /run/user/1000/podman/podman.sock:/run/user/1000/podman/podman.sock:ro --pid host --network host -it nicolargo/glances:ubuntu-dev
